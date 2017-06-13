7cfe5eea44a798eddd298af42e0f11fd66aa7d6f
Brian Dorton <dorton@gmail.com>
2017-06-13 06:50:27 -0500

    adds datepicker to announcement and cohort forms

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -28,7 +28,7 @@ class AnnouncementsController < ApplicationController
 
     respond_to do |format|
       if @announcement.save
-        format.html { redirect_to @announcement, notice: 'Announcement was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Announcement was successfully created.' }
         format.json { render :show, status: :created, location: @announcement }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class AnnouncementsController < ApplicationController
   def update
     respond_to do |format|
       if @announcement.update(announcement_params)
-        format.html { redirect_to @announcement, notice: 'Announcement was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Announcement was successfully updated.' }
         format.json { render :show, status: :ok, location: @announcement }
       else
         format.html { render :edit }
