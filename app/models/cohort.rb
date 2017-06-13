e469c893232aa77bddccafc3be7eb99d6972f4b0
Brian Dorton <dorton@gmail.com>
2017-06-13 14:51:54 -0500

    adds a destroy dependency to announcements

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -2,5 +2,5 @@ class Cohort < ApplicationRecord
   has_many :users, through: :user_cohorts
   has_many :user_cohorts, dependent: :destroy
   has_many :students
-  has_many :announcements
+  has_many :announcements, dependent: :destroy
 end
