9f8364d37c8cbfc68eb47b8be798e3153832d4e8
Brian Dorton <dorton@gmail.com>
2017-06-17 07:55:46 -0500

    limits meetups on the home page to current week.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,6 @@ class HomeController < ApplicationController
     @cohort_announcements = Announcement.where(cohort_id: @cohorts.pluck(:id))
     @weekly_challenge = CodeChallenge.weekly(@cohort.start_date)
     @announcements = @cohort_announcements.where(meetup: false)
-    @meetups = @cohort_announcements.where(meetup: true)
+    @meetups = @cohort_announcements.where(meetup: true).where(date: Date.today.beginning_of_week..Date.today.end_of_week)
   end
 end
